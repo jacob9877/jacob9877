350878036bbbc78a785df6df064c35497a66c001
Max Malik <maxzmalik@gmail.com>
2025-06-24 22:01:21 -0400

    use CORS middleware

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import logging
 import boto3
 from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from pydantic import BaseModel
 
@@ -12,6 +13,14 @@ logger = logging.getLogger(__name__)
 load_dotenv(find_dotenv(), override=True)
 
 app = FastAPI(root_path="/<stage>")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
