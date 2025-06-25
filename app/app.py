15bd02f638776239f32ea9721fc5db97c68fd467
Max Malik <maxzmalik@gmail.com>
2025-06-24 21:58:37 -0400

    change root path in attempt to expose docs

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 load_dotenv(find_dotenv(), override=True)
 
-app = FastAPI()
+app = FastAPI(root_path="/<stage>")
 
 
 @app.get("/")
