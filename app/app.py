3ff9680082ef4813a41f808162b7d22d1e257742
Max Malik <maxzmalik@gmail.com>
2025-07-05 14:21:02 -0400

    use app.routers instead of just .routers

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
-from .routers import breast_cancer_patients, users
+from app.routers import breast_cancer_patients, users
 
 load_dotenv()
 app = FastAPI()
