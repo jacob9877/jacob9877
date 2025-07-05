248c41591b42d17b8afca3f2c156b95b67eca2dd
Max Malik <maxzmalik@gmail.com>
2025-07-05 14:28:09 -0400

    add root requests

--- app/app.py
+++ app/app.py
@@ -20,4 +20,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the AI for Bariatric Surgery API!"}
+
+
+@app.get("/crash")
+def crash():
+    raise RuntimeError("This is a test error")
+
+
 handler = Mangum(app)
