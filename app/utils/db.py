b126d2aa5f701f54a83bf99bbddb7f52be44d6ba
Max Malik <maxzmalik@gmail.com>
2025-07-26 16:18:36 -0400

    use yield so we dont have to worry about closing the connection in every function

--- app/utils/db.py
+++ app/utils/db.py
@@ -6,13 +6,17 @@ from mysql.connector.cursor import MySQLCursor
 
 
 def get_db_connection() -> MySQLConnection:
-    return mysql.connector.connect(
+    conn = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         port=int(os.getenv("DB_PORT")),
         database=os.getenv("DB_NAME"),
     )
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 
 def user_exists(cursor: MySQLCursor, user_id: int) -> bool:
@@ -22,4 +26,9 @@ def user_exists(cursor: MySQLCursor, user_id: int) -> bool:
 
 def conversation_exists(cursor: MySQLCursor, conversation_id: int) -> bool:
     cursor.execute("SELECT id FROM conversations WHERE id = %s", (conversation_id,))
+    return cursor.fetchone() is not None
+
+
+def breast_cancer_patient_exists(cursor: MySQLCursor, patient_id: int) -> bool:
+    cursor.execute("SELECT id FROM breast-cancer-patients WHERE id = %s", (patient_id,))
     return cursor.fetchone() is not None
