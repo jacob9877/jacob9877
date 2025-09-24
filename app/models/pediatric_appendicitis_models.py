55e863fd076b5be89e2ef2e3e748894610e7366a
Max Malik <maxzmalik@gmail.com>
2025-09-24 14:59:17 -0400

    change appendicitis features

--- app/models/pediatric_appendicitis_models.py
+++ app/models/pediatric_appendicitis_models.py
@@ -39,42 +39,49 @@ class PresignedUpload(BaseModel):
 
 class PediatricAppendicitisPatientFeatures(BaseModel):
     # Demographic
-    Age: float | None = Field(default=None, gt=0, example=12.68)
-    BMI: float | None = Field(default=None, gt=0, example=16.90)
-    Sex: Literal["male", "female"] | None = Field(example="female")
-    Height: float | None = Field(default=None, gt=0, example=148.0)
-    Weight: float | None = Field(default=None, gt=0, example=37.0)
+    Age: float = Field(gt=0, example=12.68)
+    Sex: Literal["male", "female"] = Field(example="female")
+    Height: float = Field(gt=0, example=148.0)
+    Weight: float = Field(gt=0, example=37.0)
+    BMI: float = Field(gt=0, example=16.90)
 
     # Scoring
-    Alvarado_Score: int | None = Field(default=None, gte=0, example=4)
-    Paedriatic_Appendicitis_Score: int | None = Field(default=None, gte=0, example=3)
+    Alvarado_Score: int = Field(gte=0, example=4)
+    Paedriatic_Appendicitis_Score: int = Field(gte=0, example=3)
 
     # Clinical
-    Peritonitis: Literal["yes", "no"] | None = Field(default=None, example="no")
-    Migratory_Pain: Literal["yes", "no"] | None = Field(default=None, example="no")
-    Lower_Right_Abd_Pain: Literal["yes", "no"] | None = Field(
-        default=None, example="yes"
-    )
-    Contralateral_Rebound_Tenderness: Literal["yes", "no"] | None = Field(
-        default=None, example="yes"
-    )
-    Coughing_Pain: Literal["yes", "no"] | None = Field(default=None, example="no")
-    Nausea: Literal["yes", "no"] | None = Field(default=None, example="no")
-    Loss_of_Appetite: Literal["yes", "no"] | None = Field(default=None, example="yes")
-    Body_Temperature: float | None = Field(default=None, gte=0, example=37.00)
+    Peritonitis: Literal["yes", "no"] = Field(example="no")
+    Migratory_Pain: Literal["yes", "no"] = Field(example="no")
+    Lower_Right_Abd_Pain: Literal["yes", "no"] = Field(example="yes")
+    Contralateral_Rebound_Tenderness: Literal["yes", "no"] = Field(example="yes")
+    Ipsilateral_Rebound_Tenderness: Literal["yes", "no"] = Field(example="no")
+    Coughing_Pain: Literal["yes", "no"] = Field(example="no")
+    Psoas_Sign: Literal["yes", "no"] = Field(example="yes")
+    Nausea: Literal["yes", "no"] = Field(example="no")
+    Loss_of_Appetite: Literal["yes", "no"] = Field(example="yes")
+    Body_Temperature: float = Field(gte=0, example=37.00)
+    Dysuria: Literal["yes", "no"] = Field(example="no")
+    Stool: Literal["normal", "diarrhea", "constipation"] = Field(example="normal")
 
     # Laboratory
-    WBC_Count: float | None = Field(default=None, gte=0, example=7.70)
-    Neutrophil_Percentage: float | None = Field(default=None, gte=0, example=68.20)
-    CRP: int | None = Field(default=None, gte=0, example=0)
-    RBC_in_Urine: Literal["no", "+", "++", "+++"] | None = Field(
-        default=None, example="+"
-    )
+    WBC_Count: float = Field(gte=0, example=7.70)
+    RBC_Count: float = Field(gte=0, example=5.27)
+    Hemoglobin: float = Field(gte=0, example=14.80)
+    RDW: float = Field(gte=0, example=12.20)
+    Thrombocyte_Count: int = Field(gte=0, example=254.00)
+    Neutrophil_Percentage: float = Field(gte=0, example=68.20)
+    Neutrophilia: Literal["yes", "no"] = Field(example="no")
+    Segmented_Neutrophils: float | None = Field(default=None, gte=0, example=None)
+    CRP: int = Field(gte=0, example=0)
+    Ketones_in_Urine: Literal["no", "+", "++", "+++"] = Field(example="++")
+    RBC_in_Urine: Literal["no", "+", "++", "+++"] = Field(example="+")
+    WBC_in_Urine: Literal["no", "+", "++", "+++"] = Field(example="no")
 
-    # Ultrasound
-    US_Performed: Literal["yes", "no"] | None = Field(default=None, example="yes")
+    # Ultrasound (none required except US_Performed)
+    US_Performed: Literal["yes", "no"] = Field(example="yes")  # Required
     Appendix_on_US: Literal["yes", "no"] | None = Field(default=None, example="yes")
     Appendix_Diameter: float | None = Field(default=None, gte=0, example=7.10)
+    Free_Fluids: Literal["yes", "no"] | None = Field(default=None, example="no")
     Appendix_Wall_Layers: Literal["intact", "partially raised", "raised"] | None = (
         Field(default=None, example="intact")
     )
@@ -85,11 +92,27 @@ class PediatricAppendicitisPatientFeatures(BaseModel):
     Surrounding_Tissue_Reaction: Literal["yes", "no"] | None = Field(
         default=None, example=None
     )
+    Pathological_Lymph_Nodes: Literal["yes", "no"] | None = Field(
+        default=None, example="yes"
+    )
+    # Lymph_Node_Location not accepted due to it having no restrictions on its value
     Bowel_Wall_Thickening: Literal["yes", "no"] | None = Field(
         default=None, example=None
     )
     Ileus: Literal["yes", "no"] | None = Field(default=None, example=None)
+    Coprostasis: Literal["yes", "no"] | None = Field(default=None, example=None)
+    Meteorism: Literal["yes", "no"] | None = Field(default=None, example=None)
     Enteritis: Literal["yes", "no"] | None = Field(default=None, example=None)
+    Appendicolith: Literal["yes", "no"] | None = Field(default=None, example=None)
+    Perforation: Literal["yes", "no"] | None = Field(default=None, example="no")
+    Appendicular_Abscess: Literal["yes", "no"] | None = Field(
+        default=None, example="no"
+    )
+    # Abscess_Location not accepted due to it having no restrictions on its value
+    Conglomerate_of_Bowel_Loops: Literal["yes", "no"] | None = Field(
+        default=None, example=None
+    )
+    # Gynecological_Findings not accepted due to it having no restrictions on its value
 
 
 FEATURE_NAMES = list(PediatricAppendicitisPatientFeatures.model_fields.keys())
