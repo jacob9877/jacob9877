1018249d377ff6c30eef385770eec91409e37913
Max Malik <maxzmalik@gmail.com>
2025-10-01 23:19:55 -0400

    ++

--- app/utils/assistants/base_assistant.py
+++ app/utils/assistants/base_assistant.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 
-from app.models.conversation_models import Conversation
+from app.models.conversation_models import AssistantSlug, Conversation
 
 
 class Assistant(ABC):
+
+    @property
+    @abstractmethod
+    def assistant_name(self) -> AssistantSlug:
+        pass
 
     @abstractmethod
     def invoke(self, conversation: Conversation, user_message: str) -> str:
