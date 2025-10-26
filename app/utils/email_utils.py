fc2456db19f6238f55ceb71ad9cb353ba82ce6be
Max Malik <maxzmalik@gmail.com>
2025-10-26 17:13:19 -0500

    use & instead of ? in email

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -63,7 +63,7 @@ async def send_registration_email(
 
     if registration_role_and_condition.condition:
         registration_link += (
-            f"?condition={registration_role_and_condition.condition.value}"
+            f"&condition={registration_role_and_condition.condition.value}"
         )
 
     message = MessageSchema(
